513c5d0cf4d1fb4587ec74477efceb9bae0b9853
WanderTheWeeb <josefranciscosantesprimo@gmail.com>
2024-05-06 12:32:49 -0600

    Funcion tabla de multiplicar
    Se creo un programa por linea de comandos en Java SpringBoot a traves de la linea de comandos, en este primer commit se desarollo la primera funcion que dado un numero imprime su tabla de multiplicar
    
    Falta por anexar los ultimos dos casos  1) Mostrar las tablas de multiplicar del 2 al 5 3)Salir

---
+++ src/main/java/mx/uv/com/Practica/de/Control/de/Versiones/PracticaDeControlDeVersionesApplication.java
@@ -0,0 +1,45 @@
+package mx.uv.com.Practica.de.Control.de.Versiones;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class PracticaDeControlDeVersionesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDeControlDeVersionesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		int opc  = 0;
+		int numero ;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("1) Mostrar una tabla");
+		System.out.println("2) Mostrar tablas del 2 al 5");
+		System.out.println("3) Salir");
+
+		switch (opc) {
+			case 1:
+				System.out.println("Ingresa un numero para mostrar su tabla de multiplicar");
+				numero = sc.nextInt();
+				tabla(numero);
+				break;
+
+			default:
+				System.out.println("Opcion no valida");
+				break;
+		}
+	}
+
+	public void tabla (int numero) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(numero + " x " + i + " = " + numero * i);
+		}
+	}
+}
---
+++ src/test/java/mx/uv/com/Practica/de/Control/de/Versiones/PracticaDeControlDeVersionesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.com.Practica.de.Control.de.Versiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDeControlDeVersionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
